a625cd99f8f37af3e39d750c4445efbd4fcd0163
Amit9807 <samit9076@gmail.com>
2020-09-09 02:04:48 +0530

    Updates

---
+++ precache-manifest.e5c7b5ef339dfb26c29bdbc1fcd78d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6fdd7b80839973e5f1ce5e53a9a124",
+    "url": "/pro-organiser-application/index.html"
+  },
+  {
+    "revision": "6dd6e216371e93ced230",
+    "url": "/pro-organiser-application/static/css/main.484c7848.chunk.css"
+  },
+  {
+    "revision": "421215d5b51e243f2b62",
+    "url": "/pro-organiser-application/static/js/2.88e83492.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/pro-organiser-application/static/js/2.88e83492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dd6e216371e93ced230",
+    "url": "/pro-organiser-application/static/js/main.6160291f.chunk.js"
+  },
+  {
+    "revision": "eb1d6d4f3f03a089deea",
+    "url": "/pro-organiser-application/static/js/runtime-main.7d10b170.js"
+  },
+  {
+    "revision": "1c6e8371de87834d701f70b246b74b46",
+    "url": "/pro-organiser-application/static/media/login.1c6e8371.jpg"
+  }
+]);
